3225fdc10f7150326b09b84d34ddace7fdb2b16d
Dominique Sakrisson <domsakrisson@gmailcom>
2021-09-16 15:06:07 -0700

    Add track delete button on track page

--- src/containers/TrackListPage/TrackListPage.jsx
+++ src/containers/TrackListPage/TrackListPage.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import defaultTracks from '../../components/app/modules/DefaultSongs.js';
 import styled, { createGlobalStyle, keyframes, css } from "styled-components";
 
 const tracks = [...defaultTracks];
 // const userTracks = localStorage.getItem('trackList');
-const userTracks = JSON.parse(localStorage.getItem('trackList'));
 
 const TrackListPage = () => {
+  const [userTracks, setUserTracks] = useState(
+    (JSON.parse(localStorage.getItem('trackList'))) ? JSON.parse(localStorage.getItem('trackList')) : []);
 
+  useEffect(() => {
+    localStorage.setItem("trackList", JSON.stringify(userTracks));
+  
+  }, [userTracks]);
+  
   const DefSongList = styled.ul`
     width: 300px;
     height: 300px;	
@@ -26,6 +32,29 @@ const TrackListPage = () => {
   }
   `;
 
+  const TrackTitle = styled.div`
+    font-size: 2rem;
+    background-color: grey;
+  `;
+  const TrackButton = styled.div`
+    font-size: 1rem;
+    background-color: grey;
+    padding: 1rem;
+    border: 1px solid black;
+    max-width: 3rem;
+    box-sizing:content-box;
+    &:hover{
+      background-color: lightblue;
+      cursor: pointer;
+    }
+  `;
+
+  function handleDeleteTrack(track){
+    const updatedTracks = userTracks.filter((item) => {
+      return item.name !== track.name;
+    });
+    setUserTracks(updatedTracks);
+  }
 
   return (<>
     <h2><i>If you saved a track and it does not show, refresh page</i></h2>
@@ -34,7 +63,7 @@ const TrackListPage = () => {
       {
         tracks.map((track) => (
           <li key={track} >  
-            <h3>{track.name}</h3>
+            <TrackTitle>{track.name}</TrackTitle>
             <ul>
               {track.notes.map(note => {
                 return <li key={note}>
@@ -52,9 +81,9 @@ const TrackListPage = () => {
     </DefSongList>
     <SongList >
       <h1>Your Tracks</h1>
-      {(userTracks) ? userTracks.map(track => (
+      {(userTracks.length > 0) ? userTracks.map(track => (
         <li key={track} >  
-          <h3>{track.name}</h3>
+          <TrackTitle>{track.name}</TrackTitle>
           <ul>
             {track.recording.map(note => {
               return <li key={note.key}>
@@ -66,7 +95,9 @@ const TrackListPage = () => {
           </ul>
           <button>play track</button>
           <button>edit track</button>
-          <button>delete track</button>
+          <TrackButton onClick={() => {
+           handleDeleteTrack(track);
+          }}>delete track</TrackButton>
         </li>
       )) : '' }
       
